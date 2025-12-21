f377083d32ef54430ba62ad46232497c7d7be003
zahidmem <106247277+zahidmem@users.noreply.github.com>
2025-12-21 15:18:10 +0000

    origin update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: true,
+    origin: ["https://allgrab.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
 );
