f4d8a732de7c934292df6c0175f6dcfa57a5b419
zahidmem <106247277+zahidmem@users.noreply.github.com>
2025-12-21 19:15:47 +0000

    Cors2

--- server.js
+++ server.js
@@ -17,7 +17,24 @@ const allowedOrigins = [
   "https://allgrab.in",
 ];
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+
+  // Preflight request handle
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 // MongoDB connect
