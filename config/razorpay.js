ebb95f86680b1e5b38c0e730f11464756f6eecc1
zahidmem <106247277+zahidmem@users.noreply.github.com>
2026-01-02 19:14:57 +0000

    check

--- config/razorpay.js
+++ config/razorpay.js
@@ -2,9 +2,14 @@ import Razorpay from "razorpay";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log("KEY_ID:", process.env.RAZORPAY_KEY_ID);
+console.log("KEY_SECRET:", process.env.RAZORPAY_KEY_SECRET ? "OK" : "MISSING");
+
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
+console.log("KEY_ID:", process.env.RAZORPAY_KEY_ID);
+console.log("KEY_SECRET:", process.env.RAZORPAY_KEY_SECRET ? "OK" : "MISSING");
 
 export default razorpay;
